52f142c71181d8b6b7ed03c08ab9e26778ff77d8
saswati2 <161897421+saswati2@users.noreply.github.com>
2024-07-18 17:01:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,77 @@
-const margin = { top: 20, right: 30, bottom: 70, left: 90 },
+// Set up the SVG canvas dimensions
+const margin = {top: 30, right: 20, bottom: 70, left: 60},
       width = 960 - margin.left - margin.right,
-      height = 600 - margin.top - margin.bottom;
+      height = 500 - margin.top - margin.bottom;
 
 const svg = d3.select("#chart")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
   .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-let covidData = {};
+// Load the data
+d3.csv("aggregated_covid_data.csv").then(data => {
+    // Parse the data
+    data.forEach(d => {
+        d.Confirmed = +d.Confirmed;
+        d.year = +d.year;
+    });
 
-d3.csv("data.csv").then(data => {
-  data.forEach(d => {
-    const year = d.Date.split('-')[0];
-    if (!covidData[year]) {
-      covidData[year] = [];
-    }
-    covidData[year].push({ state: d.Province_State, cases: +d.Confirmed });
-  });
+    // Create a list of unique states and years
+    const states = [...new Set(data.map(d => d.Province_State))];
+    const years = [...new Set(data.map(d => d.year))];
 
-  function updateChart(year) {
-    const yearData = covidData[year];
+    // Set up scales
+    const x0 = d3.scaleBand()
+        .domain(states)
+        .rangeRound([0, width])
+        .padding(0.1);
 
-    const x = d3.scaleBand()
-      .domain(yearData.map(d => d.state))
-      .range([0, width])
-      .padding(0.1);
+    const x1 = d3.scaleBand()
+        .domain(years)
+        .rangeRound([0, x0.bandwidth()])
+        .padding(0.05);
 
     const y = d3.scaleLinear()
-      .domain([0, d3.max(yearData, d => d.cases)])
-      .nice()
-      .range([height, 0]);
+        .domain([0, d3.max(data, d => d.Confirmed)])
+        .nice()
+        .rangeRound([height, 0]);
 
-    svg.selectAll(".bar").remove();
-    svg.selectAll(".axis").remove();
-
-    svg.selectAll(".bar")
-      .data(yearData)
-      .enter()
-      .append("rect")
-      .attr("class", "bar")
-      .attr("x", d => x(d.state))
-      .attr("y", d => y(d.cases))
-      .attr("width", x.bandwidth())
-      .attr("height", d => height - y(d.cases))
-      .attr("fill", "steelblue");
+    // Create axes
+    svg.append("g")
+        .selectAll("g")
+        .data(states)
+        .enter().append("g")
+        .attr("transform", d => `translate(${x0(d)},0)`)
+      .selectAll("rect")
+      .data(d => years.map(y => ({year: y, data: data.find(dd => dd.Province_State === d && dd.year === y)})))
+      .enter().append("rect")
+        .attr("x", d => x1(d.year))
+        .attr("y", d => y(d.data ? d.data.Confirmed : 0))
+        .attr("width", x1.bandwidth())
+        .attr("height", d => height - y(d.data ? d.data.Confirmed : 0))
+        .attr("class", "bar");
 
     svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(x))
-      .selectAll("text")
-      .attr("transform", "rotate(-45)")
-      .style("text-anchor", "end");
-
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(d3.axisLeft(y));
-
-    svg.selectAll(".label").remove();
-
-    svg.selectAll(".label")
-      .data(yearData)
-      .enter()
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x0))
       .append("text")
-      .attr("class", "label")
-      .attr("x", d => x(d.state) + x.bandwidth() / 2)
-      .attr("y", d => y(d.cases) - 5)
-      .attr("text-anchor", "middle")
-      .text(d => d.cases.toLocaleString());
-  }
+        .attr("x", width)
+        .attr("y", margin.bottom - 10)
+        .attr("text-anchor", "end")
+        .attr("fill", "#000")
+        .text("State");
 
-  d3.select("#year-select").on("change", function() {
-    const selectedYear = d3.select(this).property("value");
-    updateChart(selectedYear);
-  });
-
-  updateChart("2020");
+    svg.append("g")
+        .attr("class", "y-axis")
+        .call(d3.axisLeft(y))
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left)
+        .attr("dy", ".71em")
+        .attr("text-anchor", "end")
+        .attr("fill", "#000")
+        .text("Confirmed Cases");
 });
